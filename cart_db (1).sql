5b14219e6d501927f6cffa9086796131bec6b336
Sphynxxxxx <larrydenverbiaco@gmail.com>
2024-11-29 13:51:41 +0800

    upsss

--- cart_db (1).sql
+++ cart_db (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2024 at 03:55 PM
+-- Generation Time: Nov 28, 2024 at 07:54 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -44,7 +44,8 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`id`, `name`, `contact_number`, `address`, `email`, `password`, `images`, `status`, `profile_image`) VALUES
-(6, 'larry', '09123456781', 'Batuan', 'larrydenverbiaco@gmail.com', '$2y$10$.6cFUjN8l7yi5/vKWmkLpum/h3WNfI7LBzvkHQ5WpMyxrZY.zZ46O', 'Cus_uploads/67472f8990fc85.72414074_download.png', 'approved', NULL);
+(8, 'Larry Denver', '09123456781', 'Batuan', 'larrydenverbiaco@gmail.com', '$2y$10$2GJDD2AKlhXYIv.FWiN1HuqWFufnA3AV27HFQiQkSLyn2SJp3Flpm', 'Cus_uploads/6748b0f92557b4.40703348_download.png', 'approved', NULL),
+(9, 'larry', '09123456781', 'Cahaguichican', 'lry4750@gmail.com', '$2y$10$3Njb6zLIZidlG7znJii11.LrOwJXOSz1WisXKUbFlK5sK615NtDBi', 'Cus_uploads/6748b127931539.41849396_download.png', 'approved', NULL);
 
 -- --------------------------------------------------------
 
@@ -69,7 +70,8 @@ CREATE TABLE `lender` (
 --
 
 INSERT INTO `lender` (`id`, `lender_name`, `contact_number`, `address`, `email`, `password`, `images`, `status`, `profile_image`) VALUES
-(7, 'larry', '09123456781', 'Batuan', 'lry4750@gmail.com', '$2y$10$JQ/8zvIMIH58P6pfkP2CMOcr3329uqiskJK5la3XeuiOxZijAZpdS', 'Cus_uploads/67473230736276.69657166_download.png', 'approved', NULL);
+(9, 'Patrick Catalan', '09123456781', 'Cahaguichican', 'larrydenverbiaco@gmail.com', '$2y$10$lSAg0cBbxwRl.ho35OZ5KuPTyXoiZARaJ6KRTbRGMMHm2A0S4T2hK', 'Cus_uploads/6748b09b1d14f9.26615684_download.png', 'approved', NULL),
+(10, 'Patrick john Catalan', '09123456781', 'Bongco', 'lry4750@gmail.com', '$2y$10$04NENO/8KU7IRo62EbeaA.uw7d8i6jOtmJIJBE3ovnVUVBQzSl7SG', 'Cus_uploads/6748b0cedd16f7.89933388_download.png', 'approved', NULL);
 
 -- --------------------------------------------------------
 
@@ -80,11 +82,20 @@ INSERT INTO `lender` (`id`, `lender_name`, `contact_number`, `address`, `email`,
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
+  `lender_id` int(11) NOT NULL,
   `order_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `delivery_method` enum('pickup','cod') NOT NULL DEFAULT 'pickup',
   `reference_number` varchar(255) NOT NULL,
   `total_price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `customer_id`, `lender_id`, `order_date`, `delivery_method`, `reference_number`, `total_price`) VALUES
+(1, 9, 0, '2024-11-28 18:26:23', 'cod', 'REF-20241128-9F32A1', 25.00),
+(2, 9, 0, '2024-11-28 18:33:57', 'pickup', 'REF-20241128-FECA4F', 24.00);
 
 -- --------------------------------------------------------
 
@@ -98,8 +109,18 @@ CREATE TABLE `order_details` (
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
   `price` decimal(10,2) NOT NULL,
-  `shippingfee` decimal(10,2) NOT NULL
+  `shippingfee` decimal(10,2) NOT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`id`, `order_id`, `product_id`, `quantity`, `price`, `shippingfee`, `start_date`, `end_date`) VALUES
+(1, 1, 24, 2, 12.00, 1.00, NULL, NULL),
+(2, 2, 24, 2, 12.00, 0.00, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -128,10 +149,7 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `product_name`, `lender_name`, `location`, `price`, `image`, `description`, `status`, `shippingfee`, `created_at`, `categories`, `quantity`, `rent_days`) VALUES
-(5, 'tool 3', 'patrick', 'Cahaguichican', 12.00, 'download.png', 'hahahha', 'approved', 12.00, '2024-11-25 17:09:28', 'Hand Tools', 0, 21),
-(6, 'tool 332', 'larryYYYYYY', 'Cahaguichican', 123.00, 'notebooks.jpg', 'SEAHJAKS', 'approved', 10.00, '2024-11-26 14:51:51', 'Harvesting Tools', 4, 111),
-(7, 'tool 111', 'patrick', 'Dapitan', 11.00, 'eraser.jpg', 'qwregfgdfgdf', 'approved', 11.00, '2024-11-26 15:34:27', 'Harvesting Tools', 9, 11),
-(8, 'tool 71', 'patrickkkkkk', 'Dapitan', 45.00, 'scissors.jpg', 'qwregfgdfgdf', 'approved', 45.00, '2024-11-26 15:48:14', 'Tilling Tools', 29, 12);
+(24, 'tool 331', 'patrickkkkkk', 'Amamaros', 12.00, 'notebooks.jpg', 'qwregfgdfgdf', 'approved', 1.00, '2024-11-28 18:07:39', 'Seeding Tools', 7, 4);
 
 --
 -- Indexes for dumped tables
@@ -182,31 +200,31 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `lender`
 --
 ALTER TABLE `lender`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Constraints for dumped tables
